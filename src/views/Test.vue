aa7ff93aca4516a9590aa8836e23b64da36c537f
pandaomeng <pandaomeng@corp-ci.com>
2017-08-01 16:09:42 +0800

    增加post数据的demo

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,72 +1,87 @@
 <template>
   <div>
-    <scroller class="scroller" append="tree" offset-accuracy="10" @scroll="scrollHandler">
-      <image class="image" src="https://gw.alicdn.com/tps/TB1dzanMVXXXXXQXVXXXXXXXXXX-573-412.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1p9CCMVXXXXa_XFXXXXXXXXXX-450-340.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1zpSiMVXXXXchXFXXXXXXXXXX-448-338.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1EuGIMVXXXXcoXpXXXXXXXXXX-452-337.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1dzanMVXXXXXQXVXXXXXXXXXX-573-412.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1p9CCMVXXXXa_XFXXXXXXXXXX-450-340.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1zpSiMVXXXXchXFXXXXXXXXXX-448-338.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1EuGIMVXXXXcoXpXXXXXXXXXX-452-337.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1dzanMVXXXXXQXVXXXXXXXXXX-573-412.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1p9CCMVXXXXa_XFXXXXXXXXXX-450-340.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1zpSiMVXXXXchXFXXXXXXXXXX-448-338.png"></image>
-      <image class="image" src="https://gw.alicdn.com/tps/TB1EuGIMVXXXXcoXpXXXXXXXXXX-452-337.png"></image>
-    </scroller> 
-    <text class = "event-cnt">{{eventCnt}}</text>
-    <text class = "hint">{{hint}}</text>
+    <text style="background-color:#ccc;padding:10px;width:100px;font-size:28px" @click="postMessage()">点我动起</text>
+
+    <transition enter-class="slide-fade-enter" mode="out-in" @enter="enter" @leave="leave">
+      <div class="square" :style="{backgroundColor:show}" :key="show">
+        <text class="text">{{show}}</text>
+      </div>
+    </transition>
+
   </div>
 </template>
 
-<script>
-  export default  {
-    data () {
-      return {
-        eventCnt: 0,
-        hint:"start",
-      }
-    },
-    methods: {
-      scrollHandler: function(e) {
-        console.log("here")
-        var nowCnt = this.eventCnt + 1;
-        this.eventCnt = nowCnt;
-        this.offsetX = e.contentOffset.x;
-        this.offsetY = e.contentOffset.y;
-        this.hint = "scroll event count:" + this.eventCnt + " offsetX:" + this.offsetX + " offsetY:" +this.offsetY;
-      }
-    }
-  };
-</script>
-
-<style scoped>
-  .body {
-    background-color: #ffffff;
+<style>
+  .text {
+    font-size: 50px;
   }
-  .image {
-    width:600px;
-    background-color:#FFFFDF;
-    height:400px;
-  }
-  .scroller {
-    width: 600px;
-    height:800px;
+  .square{
+    width:400px;height:400px;
+    justify-content: center;
+    align-items:center;
   }
 
-  .hint {
-    position: absolute;
-    width: 750px;
-    height: 150px;
-    top: 900px;
-    left: 0px;
-  }
-  .event-cnt {
-    position: absolute;
-    width: 750px;
-    height: 150px;
-    top: 1050px;
-    left: 0px;
-  }
+  .slide-fade-enter{
+  transform: translateX(40px);
+  opacity: 0;
+}
+
 </style>
 
+<script>
+  const animation = weex.requireModule('animation')
+  const stream = weex.requireModule('stream')
+
+  function slider (action, el, callback = () => {}) {
+    const x = action === 'out' ? '40px' : '0'
+    animation.transition(el, {
+      styles: {
+        transform: 'translateX('+ x +')',
+        opacity: action === 'out' ? 0 : 1
+      },
+      duration: 500,
+      timingFunction: 'ease-' + action,
+      delay: 0
+    }, callback)
+  }
+
+  export default {
+    data () {
+      return {
+        show:'red'
+      }
+    },
+    methods:{
+      postMessage () {
+        // this.show = this.show === 'red'?'blue':'red'
+
+        var totalData=  [{"data":[{"channelNum":0,"pressure":13,"pressureState":64},{"channelNum":1,"pressure":16776700,"pressureState":65}],"time":1501558058},{"data":[{"channelNum":0,"pressure":12,"pressureState":64},{"channelNum":1,"pressure":16776699,"pressureState":65}],"time":1501558558}]
+        console.log(totalData)
+        var postData = 'device_id=12&&data='+JSON.stringify(totalData)
+        console.log(postData)
+        stream.fetch({
+          method: 'POST',
+          type: 'json',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          url: 'https://app-test1.baichengyiliao.com/main.php/jf/measure/upload.json', //前面的部分
+          // url: 'http://127.0.0.1/json.php',
+          body: postData
+        }, function(res){
+          console.log(res)
+          console.log(res.data)
+          console.log(res.data == "success")
+          console.log(res.data.data == "success")
+
+        })
+      },
+      enter: function (el, done) {
+        slider('in', el.__vue__, done)
+      },
+      leave: function (el, done) {
+        slider('out', el.__vue__, done)
+      },
+    }
+  }
+</script>
