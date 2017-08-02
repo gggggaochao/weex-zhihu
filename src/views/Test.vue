37a008ddd0ecdb85f4c5d74bea4d340edce1a18a
pandaomeng <pandaomeng@corp-ci.com>
2017-08-02 10:15:50 +0800

    增加http-build-query模块

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -31,7 +31,7 @@
 <script>
   const animation = weex.requireModule('animation')
   const stream = weex.requireModule('stream')
-
+  import httpBuildQuery from 'http-build-query'
   function slider (action, el, callback = () => {}) {
     const x = action === 'out' ? '40px' : '0'
     animation.transition(el, {
@@ -57,22 +57,22 @@
 
         var totalData=  [{"data":[{"channelNum":0,"pressure":13,"pressureState":64},{"channelNum":1,"pressure":16776700,"pressureState":65}],"time":1501558058},{"data":[{"channelNum":0,"pressure":12,"pressureState":64},{"channelNum":1,"pressure":16776699,"pressureState":65}],"time":1501558558}]
         console.log(totalData)
-        var postData = 'device_id=12&&data='+JSON.stringify(totalData)
-        console.log(postData)
+        // var postData = 'device_id=12&&data='+JSON.stringify(totalData)
+        var postData = {
+          device_id: 12,
+          data: totalData
+        }
+        console.log("postData: "+JSON.stringify(postData))
         stream.fetch({
           method: 'POST',
           type: 'json',
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
           },
-          url: 'https://app-test1.baichengyiliao.com/main.php/jf/measure/upload.json', //前面的部分
-          // url: 'http://127.0.0.1/json.php',
-          body: postData
+          // url: 'https://app-test1.baichengyiliao.com/main.php/jf/measure/upload.json', //前面的部分
+          url: 'http://localhost/json.php',
+          body: httpBuildQuery(postData)
         }, function(res){
-          console.log(res)
-          console.log(res.data)
-          console.log(res.data == "success")
-          console.log(res.data.data == "success")
 
         })
       },
